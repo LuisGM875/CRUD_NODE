86494280224eab36722d8395e9ea67e7f37f354e
LuisGM875 <luis_1322134149@uptecamac.edu.mx>
2024-06-30 12:32:28 -0600

    Initial commit

---
+++ src/test/java/com/crud/node/NodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.node;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
